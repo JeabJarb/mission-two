4998c1ff601fd12691d18c04c0926724c0d9202e
JeabJarb <tassanaruck.k@gmail.com>
2024-06-04 22:50:44 +1200

    add sounds

--- script.js
+++ script.js
@@ -17,6 +17,33 @@ const add5AutoDisplayEl = document.getElementById("add5AutoCount")
 const add100AutoDisplayEl = document.getElementById("add100AutoCount")
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// ========== Sounds Zone ========== //
+const clickSound = document.getElementById("soundClick")
+const popSound = document.getElementById("soundPop")
+const cancelSound = document.getElementById("soundCancel")
+const coinsSound = document.getElementById("soundCoins")
+const backgroundSound = document.getElementById("soundBackground")
+
+// === Playback === //
+clickSound.playbackRate = 6.5;
+popSound.playbackRate = 4;
+cancelSound.playbackRate = 6.5;
+coinsSound.playbackRate = 1;
+
+// === Volume === //
+backgroundSound.volume = 0.5
+coinsSound.volume = 0.5
+
+// === Bacground Music === //
+backgroundSound.loop = true;
+backgroundSound.play()
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // ========== Themes Zone ========== //
 const kiwiBrown = "./assests/kiwi1.png";
@@ -55,8 +82,8 @@ function changeCroissant() {
     
 }
 
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // ========== Function areas ========== //
@@ -90,9 +117,11 @@ function whenClick() {
         ],{
             duration: 50,
         })
-
-    }) 
+    
+    })
+    clickSound.play();
 }
+
 
 
 // ========== aad 1 click ========== //
@@ -100,12 +129,15 @@ let add1ClickCount = 0; // this will multiply per click by +1
 function add1Click() {
     // check if there are enough point to purchase the +1 click
     if (clickCount >= 50) {
+        popSound.play()
+        coinsSound.play()
         clickCount -= 50; // buy 50 points
         multiplier += 1 // increase points per click by 1
         add1ClickCount ++ // increase +1 click purchase count
         totalPoints() // update total points display
-        add1ClickDisplayEl.textContent = add1ClickCount // update the display for +1 click count
+        add1ClickDisplayEl.textContent = add1ClickCount // update click count by 1
     } else {
+        cancelSound.play()
         console.log("Not enough points!");
         // add "not enough point effect"
         document.getElementById("add1ClickButton").style.background = "red";
@@ -114,19 +146,25 @@ function add1Click() {
             document.getElementById("add1ClickButton").style.background = "";
         }, 100)
     }
+    
 }
 
 
 // ========== aad 1 auto ========== //
 let add1AutoCountPerSec = 0;
+let add1AutoClickCount = 0
 function add1Auto() {
     // check if there are enough point to purchase the +1 kiwi per second
     if (clickCount >= 100) {
+        popSound.play()
+        coinsSound.play()
         clickCount -= 100; // buy 100 points
         add1AutoCountPerSec += 1 // increase earning point per second by 1 kiwi
         totalPoints() // update total points display
-        add1AutoDisplayEl.textContent = add1AutoCountPerSec // update the display for +1 kiwi per second
+        add1AutoClickCount ++
+        add1AutoDisplayEl.textContent = add1AutoClickCount // update click count by 1
     } else {
+        cancelSound.play()
         console.log("Not enough points!");
         // add "not enough point effect"
         document.getElementById("add1AutoButton").style.background = "red";
@@ -150,12 +188,15 @@ let add5ClickCount = 0;
 function add5Click() {
     // check if there are enough point to purchase the +5 click
     if (clickCount >= 500) {
+        popSound.play()
+        coinsSound.play()
         clickCount -= 500; // buy 500 points
         multiplier += 5 // increase points per click by 5
         add5ClickCount ++ // increase +5 click purchase count
         totalPoints() // update total points display
         add5ClickDisplayEl.textContent = add5ClickCount // update the display for +5 click count
     } else {
+        cancelSound.play()
         console.log("Not enough points!");
         // add "not enough point effect"
         document.getElementById("add5ClickButton").style.background = "red";
@@ -169,14 +210,19 @@ function add5Click() {
 
 // ========== aad 5 Auto ========== //
 let add5AutoCountPerSec = 0;
+let add5AutoClickCount = 0;
 function add5Auto() {
     // check if there are enough point to purchase the +5 kiwi per second
     if (clickCount >= 1000) {
+        popSound.play()
+        coinsSound.play()
         clickCount -= 1000; // buy 1000 points
         add5AutoCountPerSec += 5 // increase earning point per second by 5 kiwi
         totalPoints() // update total points display
-        add5AutoDisplayEl.textContent = add5AutoCountPerSec // update the display for +5 kiwi per second
+        add5AutoClickCount ++
+        add5AutoDisplayEl.textContent = add5AutoClickCount // update click count by 1
     } else {
+        cancelSound.play()
         console.log("Not enough points!");
         // add "not enough point effect"
         document.getElementById("add5AutoButton").style.background = "red";
@@ -197,14 +243,19 @@ setInterval(function() {
 
 // ========== aad 100 Auto ========== //
 let add100AutoCountPerSec = 0;
+let add100AutoClickCount = 0
 function add100Auto() {
     // check if there are enough point to purchase the +100 kiwi per second
     if (clickCount >= 10000) {
+        popSound.play()
+        coinsSound.play()
         clickCount -= 10000; // buy 10000 points
         add100AutoCountPerSec += 100 // increase earning point per second by 100 kiwi
         totalPoints() // update total points display
-        add100AutoDisplayEl.textContent = add100AutoCountPerSec // update the display for +100 kiwi per second
+        add100AutoClickCount ++
+        add100AutoDisplayEl.textContent = add100AutoClickCount // update click count by 1
     } else {
+        cancelSound.play()
         console.log("Not enough points!");
         // add "not enough point effect"
         document.getElementById("add100AutoButton").style.background = "red";
